1484d927f5bcc162184d5604d0fa59c9465ab509
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-25 23:18:53 -0800

    bottom navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
+        link: "hsl(var(--link))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
