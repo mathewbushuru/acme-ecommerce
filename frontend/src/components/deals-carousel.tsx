e80109f80a31d798dd9ef9160da1364305d87490
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-27 04:21:30 -0800

    Use smaller width images

--- frontend/src/components/deals-carousel.tsx
+++ frontend/src/components/deals-carousel.tsx
@@ -56,43 +56,43 @@ export default function DealsCarousel() {
 
 const dealItems = [
   {
-    url: "https://images.unsplash.com/photo-1498557850523-fd3d118b962e?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1498557850523-fd3d118b962e?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Blueberries - Fresh, 1 pint, 1 each",
     specialPrice: "$3.99",
     retailPrice: "$7.99",
   },
   {
-    url: "https://plus.unsplash.com/premium_photo-1664472658489-8bb2cf572db1?q=80&w=2742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://plus.unsplash.com/premium_photo-1664472658489-8bb2cf572db1?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Meat Lasagna - 907 Gram",
     specialPrice: "$7.49",
     retailPrice: "$11.69",
   },
   {
-    url: "https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Ancient Grains Bread - 600 Gram",
     specialPrice: "$3.99",
     retailPrice: "$7.99",
   },
   {
-    url: "https://images.unsplash.com/photo-1494597564530-871f2b93ac55?q=80&w=2913&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1494597564530-871f2b93ac55?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Maple & Brown Sugar Flavour Oat Meal",
     specialPrice: "$3.00",
     retailPrice: "$4.79",
   },
   {
-    url: "https://images.unsplash.com/photo-1517456944721-229d38679dfa?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1517456944721-229d38679dfa?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Breakfast Cereal - 850 Gram",
     specialPrice: "$8.99",
     retailPrice: "$12.49",
   },
   {
-    url: "https://images.unsplash.com/photo-1621447504864-d8686e12698c?q=80&w=2869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1621447504864-d8686e12698c?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Sea Salt &  Vinegar Potato Chips - 200 Gram",
     specialPrice: "$3.79",
     retailPrice: "$5.49",
   },
   {
-    url: "https://images.unsplash.com/photo-1581098365948-6a5a912b7a49?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://images.unsplash.com/photo-1581098365948-6a5a912b7a49?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Coca-Cola Soft Drinks - 12 x 355ml",
     specialPrice: "$8.69",
     retailPrice: "$12.49",
