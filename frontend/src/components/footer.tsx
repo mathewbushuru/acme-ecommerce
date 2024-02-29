5199138efa1c712831c78538b526c507224abfc6
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-28 19:17:13 -0800

    Sheet component

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,22 +1,21 @@
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
 
 export default function Footer() {
   return (
     <div className="mt-8 bg-secondary p-4">
       <div className="mx-auto max-w-screen-xl px-6">
-        <p className="mb-2 text-center text-sm text-secondary-foreground">
-          This is a dev preview for a project I'm working on that contains
-          building blocks for digital commerce. <ProjectInfoSheet />
+        <p className="mb-2  text-center text-sm text-secondary-foreground ">
+          <span className="hidden sm:inline">
+            This is a dev preview for a project I'm working on that contains
+            building blocks for digital commerce.
+          </span>{" "}
+          <ProjectInfoSheet />
         </p>
         <p className="text-center text-sm text-secondary-foreground">
           <span className="cursor-pointer">
@@ -36,54 +35,35 @@ function ProjectInfoSheet() {
           Click here to learn more
         </span>
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent size="lg">
         <SheetHeader>
           <SheetTitle>Building blocks for digital commerce</SheetTitle>
-          <SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4 antialiased">
+          <p className="text-sm ">
             This project aims to create commerce modules and tools to build
             rich, reliable and performant commerce applications without
             reinventing core commerce logic.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <p className="text-sm text-muted-foreground">
-            I'm building modularized commerce logic like carts, products,
-            pricing, order management, and tools you can use to build powerful
-            ecommerce websites
           </p>
-          <p className="text-sm text-muted-foreground">
-            These modules are portable such that you can use them independently
-            in your application. The modules will be developed to work with a
-            JavaScript frontend(starting out with ReactJS and NextJS. Vue and
-            Svelte support coming later). Backend modules will be provided for
-            Node(TypeScript/JavaScript) with support for Java(SpringBoot) and
-            Python(Django) coming later.
+          <p className="text-sm">
+            It contains modularized commerce logic like carts, products,
+            authentication, pricing, order management, and tools you can use to
+            build powerful ecommerce websites.
           </p>
-          <p className="text-sm text-muted-foreground">
-            If you are looking for a fully hosted solution, I will be provided a
-            Platform As A Service Offering(PAAS) for a fee.
-          </p>
-          <p className="text-sm">Building Blocks</p>
-          <ol className="grid gap-4 py-4">
-            <li className="text-sm">
-              1. Product module - Manage product information, build categories,
-              and more.
-            </li>
-            <li className="text-sm">
-              2. Pricing Module - Define granular pricing rules and display the
-              correct price every time.
-            </li>
-            <li className="text-sm">
-              3. Inventory module - Get your stock levels right - across
-              locations, in real-time.
-            </li>
-          </ol>
+          <p className="text-sm">Building Blocks:</p>
+          <li className="text-sm">
+            Product module - Manage product information, build categories, and
+            more.
+          </li>
+          <li className="text-sm">
+            Pricing Module - Define granular pricing rules and display the
+            correct price every time.
+          </li>
+          <li className="text-sm">
+            Inventory module - Get your stock levels right, across locations and
+            in real-time.
+          </li>
         </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button>Go back</Button>
-          </SheetClose>
-        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -1,138 +1,266 @@
-import * as React from "react"
-import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { cva, type VariantProps } from "class-variance-authority"
-import { X } from "lucide-react"
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-alert-dialog";
+import { cva, type VariantProps } from "class-variance-authority";
+import { X } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Sheet = SheetPrimitive.Root
+const Sheet = SheetPrimitive.Root;
 
-const SheetTrigger = SheetPrimitive.Trigger
+const SheetTrigger = SheetPrimitive.Trigger;
 
-const SheetClose = SheetPrimitive.Close
+const portalVariants = cva("fixed inset-0 z-50 flex", {
+  variants: {
+    position: {
+      top: "items-start",
+      bottom: "items-end",
+      left: "justify-start",
+      right: "justify-end",
+    },
+  },
+  defaultVariants: {
+    position: "right",
+  },
+});
 
-const SheetPortal = SheetPrimitive.Portal
+interface SheetPortalProps
+  extends SheetPrimitive.DialogPortalProps,
+    VariantProps<typeof portalVariants> {}
+
+const SheetPortal = ({ position, children, ...props }: SheetPortalProps) => {
+  return (
+    <SheetPrimitive.Portal {...props}>
+      <div className={portalVariants({ position })}>{children}</div>
+    </SheetPrimitive.Portal>
+  );
+};
 
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Overlay
-    className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-    ref={ref}
-  />
-))
-SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
+>(({ className, children, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Overlay
+      ref={ref}
+      className={cn(
+        "fixed inset-0 bg-background/80 backdrop-blur-sm transition-all duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 
-const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+const SheetVariants = cva(
+  "fixed z-50 scale-100 bg-background p-6 opacity-100 shadow-lg",
   {
     variants: {
-      side: {
-        top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
-        bottom:
-          "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
-        right:
-          "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+      position: {
+        top: "animate-in slide-in-from-top w-full duration-300 border",
+        bottom: "animate-in slide-in-from-bottom w-full duration-300 border",
+        left: "animate-in slide-in-from-left h-full duration-300",
+        right: "animate-in slide-in-from-right h-full duration-300",
+      },
+      size: {
+        content: "",
+        default: "",
+        sm: "",
+        lg: "",
+        xl: "",
+        full: "",
       },
     },
+    compoundVariants: [
+      {
+        position: ["top", "bottom"],
+        size: "default",
+        class: "h-1/2 sm:h-1/3",
+      },
+      {
+        position: ["top", "bottom"],
+        size: "content",
+        class: "max-h-screen",
+      },
+      {
+        position: ["top", "bottom"],
+        size: "sm",
+        class: "h-1/4",
+      },
+      {
+        position: ["top", "bottom"],
+        size: "lg",
+        class: "h-1/2",
+      },
+      {
+        position: ["top", "bottom"],
+        size: "xl",
+        class: "h-5/6",
+      },
+      {
+        position: ["top", "bottom"],
+        size: "full",
+        class: "h-screen",
+      },
+      {
+        position: ["right", "left"],
+        size: "default",
+        class: "w-5/6 sm:w-1/3",
+      },
+      {
+        position: ["right", "left"],
+        size: "content",
+        class: "max-w-screen",
+      },
+      {
+        position: ["right", "left"],
+        size: "sm",
+        class: "w-2/3 sm:w-1/4",
+      },
+      {
+        position: ["right", "left"],
+        size: "lg",
+        class: "w-5/6 sm:w-1/2",
+      },
+      {
+        position: ["right", "left"],
+        size: "xl",
+        class: "w-5/6",
+      },
+      {
+        position: ["right", "left"],
+        size: "full",
+        class: "w-screen",
+      },
+    ],
     defaultVariants: {
-      side: "right",
+      position: "right",
+      size: "default",
     },
-  }
-)
+  },
+);
 
-interface SheetContentProps
+export interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof SheetVariants> {}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
-    <SheetPrimitive.Content
-      ref={ref}
-      className={cn(sheetVariants({ side }), className)}
-      {...props}
-    >
-      {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
-    </SheetPrimitive.Content>
-  </SheetPortal>
-))
-SheetContent.displayName = SheetPrimitive.Content.displayName
+>(({ position, size, className, children, ...props }, ref) => {
+  return (
+    <SheetPortal position={position}>
+      <SheetOverlay />
+      <SheetPrimitive.Content
+        ref={ref}
+        className={cn(SheetVariants({ position, size }), className)}
+        {...props}
+      >
+        {children}
+        <SheetPrimitive.Cancel className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity focus-within:ring-ring hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-background">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </SheetPrimitive.Cancel>
+      </SheetPrimitive.Content>
+    </SheetPortal>
+  );
+});
 
 const SheetHeader = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-2 text-center sm:text-left",
-      className
-    )}
-    {...props}
-  />
-)
-SheetHeader.displayName = "SheetHeader"
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col space-y-2 text-center sm:text-left",
+        className,
+      )}
+      {...props}
+    />
+  );
+};
 
 const SheetFooter = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
-    {...props}
-  />
-)
-SheetFooter.displayName = "SheetFooter"
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+const SheetAction = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Action>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Action>
+>(({ className, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Action
+      ref={ref}
+      className={cn(
+        "flex flex-col-reverse items-stretch sm:flex-row sm:items-center sm:justify-end sm:space-x-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+
+const SheetCancel = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Cancel>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Cancel>
+>(({ className, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Cancel
+      ref={ref}
+      className={cn(
+        "flex flex-col-reverse items-stretch sm:flex-row sm:items-center sm:justify-end sm:space-x-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 
 const SheetTitle = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Title
-    ref={ref}
-    className={cn("text-lg font-semibold text-foreground", className)}
-    {...props}
-  />
-))
-SheetTitle.displayName = SheetPrimitive.Title.displayName
+>(({ className, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Title
+      ref={ref}
+      className={cn("text-lg font-semibold text-foreground", className)}
+      {...props}
+    />
+  );
+});
 
 const SheetDescription = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Description
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-SheetDescription.displayName = SheetPrimitive.Description.displayName
+>(({ className, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Description
+      ref={ref}
+      className={cn("text-sm text-muted-foreground", className)}
+      {...props}
+    />
+  );
+});
 
 export {
   Sheet,
-  SheetPortal,
-  SheetOverlay,
   SheetTrigger,
-  SheetClose,
   SheetContent,
   SheetHeader,
   SheetFooter,
+  SheetAction,
+  SheetCancel,
   SheetTitle,
   SheetDescription,
-}
+};
