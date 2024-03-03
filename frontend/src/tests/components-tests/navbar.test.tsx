d60729666ba699b8daaa9e906f17a71beada7769
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-02 18:11:28 -0800

    Add mocks for FE tests

--- frontend/src/tests/components-tests/navbar.test.tsx
+++ frontend/src/tests/components-tests/navbar.test.tsx
@@ -1,9 +1,18 @@
-import { expect, describe, it, afterEach } from "vitest";
+import { expect, describe, it, afterEach, beforeEach, vi } from "vitest";
 import { cleanup, render } from "@testing-library/react";
 
 import Navbar from "@/components/navbar";
 
-describe("<Navbar />", () => {
+vi.mock("react-router-dom");
+vi.mock("@/store/store")
+
+describe("<Navbar />", async () => {
+  const rrd = await import("react-router-dom");
+
+  beforeEach( () => {
+    rrd.useNavigate = vi.fn();
+  })
+
   afterEach(cleanup);
 
   it("renders without crashing", () => {
