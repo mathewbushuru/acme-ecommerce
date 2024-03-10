9343fd273ca5deb3faa444f34dd552a980843d06
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-10 12:05:20 -0700

    JavaPos

---
+++ javaPos/JavaPos.java
@@ -0,0 +1,9 @@
+// javac JavaPos.java && java JavaPos
+
+package javaPos;
+
+class JavaPos {
+    public static void main(String[] args){
+        System.out.println("Welcome to Acme POS");
+    }
+}
