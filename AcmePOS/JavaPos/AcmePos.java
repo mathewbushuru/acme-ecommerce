4ccaf55b83fdc9d70f1f4ced3814bb55dcc60925
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-10 15:40:33 -0700

    Abstract out POS creteContent method

--- AcmePOS/JavaPos/AcmePos.java
+++ AcmePOS/JavaPos/AcmePos.java
@@ -1,16 +1,20 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
 
 public class AcmePos extends Application {
-    @Override
-    public void start(Stage stage){
+    private Parent createContent(){
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Javafx " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        return new StackPane(new Label("ACME POS. Uses JavaFX " + javafxVersion + ", running on Java " + javaVersion + "."));
+    }
+
+    @Override
+    public void start(Stage stage){
+        Scene scene = new Scene(createContent(), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
