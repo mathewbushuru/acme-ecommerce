06717b5876fd81f11540ce8b20d4c9dd25c52b88
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-25 13:09:14 -0700

    Localhost mysql

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Acme Groceries API running on port ${port}`);
+  console.log(`Acme Groceries API (localhost MySQL) running on port ${port}`);
 });
