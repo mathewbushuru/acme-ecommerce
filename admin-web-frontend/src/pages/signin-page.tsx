19a27bf408954f732c5a17fffa2ae18dd3c72e65
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-31 16:33:29 -0700

    Add max width for extra wide desktop screens

--- admin-web-frontend/src/pages/signin-page.tsx
+++ admin-web-frontend/src/pages/signin-page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/login-form";
 
 export default function SigninPage() {
   return (
-    <div className="p-4 sm:p-8 pt-52 sm:pt-40">
+    <div className="p-4 sm:p-8 pt-52 sm:pt-40 mx-auto max-w-screen-2xl">
       <LoginForm />
     </div>
   );
