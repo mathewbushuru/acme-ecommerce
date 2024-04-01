fa407e838d56f7f6c15c43a83392a6f27e8e4163
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-04-01 13:50:49 -0700

    AcmePos desktop app refactor

--- backoffice-desktop-apps/acme-pos-dotnet/Program.cs
+++ backoffice-desktop-apps/acme-pos-dotnet/Program.cs
@@ -6,6 +6,6 @@ static class Program
     static void Main()
     {
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        Application.Run(new AcmePosLoginForm());
     }
 }
