2186c00bbba769e418bf3ac2807b32846c85e8b0
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-04-17 13:46:20 -0700

    Product add new

--- admin-web-frontend/src/pages/products-page.tsx
+++ admin-web-frontend/src/pages/products-page.tsx
@@ -1,7 +1,7 @@
 import RootLayout from "@/layouts/root-layout";
 
 import ProductPageSidebar from "@/components/product-page-sidebar";
-import { Button } from "@/components/ui/button";
+import ProductAddNew from "@/components/product-add-new";
 
 export default function ProductsPage() {
   return (
@@ -13,26 +13,8 @@ export default function ProductsPage() {
         </div>
 
         {/* Page Content  */}
-        <main className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div>
-            <h1 className="text-lg font-semibold md:text-xl">
-              Inventory Maintenance
-            </h1>
-            <p className="mt-2 text-sm text-muted-foreground">
-              View product details, or enter 'Edit Mode' to edit product details
-              / add new products.
-            </p>
-          </div>
-
-          <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-xl font-bold">You have no products.</h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
+        <main className="p-4 lg:p-6">
+          <ProductAddNew />
         </main>
       </div>
     </RootLayout>
