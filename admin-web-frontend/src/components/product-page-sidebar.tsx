b7264f7e0a0215ac40cae8aca5047478d6e474ab
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-04-17 13:41:12 -0700

    Project page sidebar

---
+++ admin-web-frontend/src/components/product-page-sidebar.tsx
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import { Pencil } from "lucide-react";
+
+import ContactSupportCard from "@/components/contact-support-card";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge"
+
+export default function ProductPageSidebar() {
+    const navigate = useNavigate();
+    return (
+      <div className="flex h-full max-h-screen flex-col gap-2 border-r">
+        <div className="flex border-b px-2 py-3 lg:px-4">
+          <Button variant="secondary" size="sm" className="w-full justify-start">
+            <Pencil className="mr-2 h-4 w-4" />
+            Enter Edit Mode
+          </Button>
+        </div>
+        <div className="flex-1">
+          <nav className="flex flex-col gap-2 px-2 lg:px-4">
+            <Button
+              variant="default"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              Products
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              Product Specials
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              Product Categories
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              Purchase Orders
+              <Badge
+                variant="secondary"
+                className="ml-auto flex h-5 w-5 shrink-0 items-center justify-center rounded-full"
+              >
+                3
+              </Badge>
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              CSV Imports
+            </Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="justify-start"
+              onClick={() => navigate("#")}
+            >
+              Groups Setup
+            </Button>
+          </nav>
+        </div>
+        <div className="mt-auto p-4">
+          <ContactSupportCard />
+        </div>
+      </div>
+    );
+  }
