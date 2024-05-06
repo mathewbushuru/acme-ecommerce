bfbea7498760532e9cbf8fa3ca62fd943837a475
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-05-06 13:57:42 -0700

    Product maintenance page - search sku

--- admin-web-frontend/src/modules/products/components/product-maintenance.tsx
+++ admin-web-frontend/src/modules/products/components/product-maintenance.tsx
@@ -1,15 +1,28 @@
-import ProductNavigation from "./product-navigation";
+import { Search } from "lucide-react";
+
+import ProductNavigation from "@/modules/products/components/product-navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function ProductMaintenance() {
-  return <ProductsSubLayout>ProductMaintenance</ProductsSubLayout>;
+  return (
+    <ProductsSubLayout>
+      <div className="">
+        <SearchProduct />
+      </div>
+    </ProductsSubLayout>
+  );
 }
 
 function ProductsSubLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full flex-col gap-4 lg:gap-6">
       <div>
-        <ProductNavigation activeLink="products-home" />
-        <h1 className="text-lg font-semibold md:text-xl">Product Maintenance</h1>
+        <ProductNavigation activeLink="product-maintenance" />
+        <h1 className="text-lg font-semibold md:text-xl">
+          Product Maintenance
+        </h1>
         <p className="mt-2 text-sm text-muted-foreground">
           Manage your products and edit their details.
         </p>
@@ -19,5 +32,28 @@ function ProductsSubLayout({ children }: { children: React.ReactNode }) {
         {children}
       </main>
     </div>
+  );
+}
+
+function SearchProduct() {
+  return (
+    <form
+      className="flex-1 sm:flex-initial"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <div className="flex items-center gap-2">
+        <Label>Sku:</Label>
+        <div className="relative flex-1">
+          <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            id="sku"
+            placeholder="By Sku# or '%' + 'product_name'..."
+            className="h-9 pl-8 sm:w-[300px] md:w-[240px] lg:w-[300px]"
+          />
+        </div>
+      </div>
+      <Button className="mt-4" variant="outline">Search</Button>
+    </form>
   );
 }
