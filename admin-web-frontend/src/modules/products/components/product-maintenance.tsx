b6dea05cc5f88fbdd1c83459b284c3f4c98e9d0d
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-05-07 23:42:10 -0700

    Product Maintenance Search page

--- admin-web-frontend/src/modules/products/components/product-maintenance.tsx
+++ admin-web-frontend/src/modules/products/components/product-maintenance.tsx
@@ -5,12 +5,14 @@ import { useGetAllCategoriesQuery } from "@/api";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 
 export default function ProductMaintenance() {
@@ -21,69 +23,160 @@ export default function ProductMaintenance() {
 
   return (
     <ProductsSubLayout>
-      <form className="grid gap-2" onSubmit={() => null}>
-        <div className="flex items-center gap-2">
-          <Label className="w-40">Sku Number:</Label>
-          <div className="relative flex-1">
-            <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              id="sku"
-              defaultValue={skuSearchParam || ""}
-              placeholder="By Sku# or '%' + 'product_name'..."
-              className="h-9 pl-8 sm:w-[300px] md:w-[240px] lg:w-[300px]"
-            />
+      <form
+        className="grid h-full grid-cols-1 grid-rows-[2] sm:grid-cols-2"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        {/* Top left*/}
+        <div className="flex flex-col gap-2 ">
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Sku Number:</Label>
+            <div className="relative flex-1">
+              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                id="sku"
+                defaultValue={skuSearchParam || ""}
+                placeholder="By Sku# or '%' + 'product_name'"
+                className="h-9 w-72 pl-8"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Vendor ordering code:</Label>
+            <div className="relative flex-1">
+              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                id="vendor-ordering-code"
+                placeholder="Might be similar to sku#"
+                className="h-9 w-72 pl-8"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Vendor ID:</Label>
+            <div className="relative flex-1">
+              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                id="vendor-id"
+                placeholder="Vendor's code in system"
+                className="h-9 w-72 pl-8"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Variant group code:</Label>
+            <div className="relative flex-1">
+              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                id="variant-group-code"
+                placeholder="Mix and match items, same price"
+                className="h-9 w-72 pl-8"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Product Category:</Label>
+            <Select>
+              <SelectTrigger className="w-72">
+                <SelectValue placeholder="Select category" />
+              </SelectTrigger>
+              <SelectContent>
+                {!categoryData ? (
+                  <SelectItem value="loading"> Loading... </SelectItem>
+                ) : (
+                  <>
+                    <SelectItem value="All"> All </SelectItem>
+                    {categoryData.allCategories.map((category) => (
+                      <SelectItem value={category.name} key={category.id}>
+                        {category.name}
+                      </SelectItem>
+                    ))}
+                  </>
+                )}
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Product Name:</Label>
+            <div className="relative flex-1">
+              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                id="product-name"
+                placeholder="Quick search by '%product_name'"
+                className="h-9 w-72 pl-8"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start  gap-2 sm:flex-row sm:items-center">
+            <Label className="w-40">Product Status:</Label>
+            <Select>
+              <SelectTrigger className="w-72">
+                <SelectValue placeholder="Select status" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="All">All</SelectItem>
+                <SelectItem value="Active">Active</SelectItem>
+                <SelectItem value="Draft">Draft</SelectItem>
+                <SelectItem value="Discontinued">Discontinued</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="flex-1">&nbsp;</div>
+        </div>
+
+        {/* Top right */}
+        <div className="mt-2 hidden flex-col gap-4 sm:flex">
+          <div className="flex items-center gap-2">
+            <Checkbox id="is-on-special" />
+            <Label htmlFor="is-on-special">Product is on special?</Label>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Checkbox id="is-oos" />
+            <Label htmlFor="is-oos">Product is out of stock?</Label>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Checkbox id="is-house-brand" />
+            <Label htmlFor="is-house-brand">Product is house brand?</Label>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Checkbox id="is-house-brand" defaultChecked />
+            <Label htmlFor="is-house-brand">
+              Product adds to customer loyalty points?
+            </Label>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Checkbox id="is-house-brand" />
+            <Label htmlFor="is-house-brand">Among top 100 sellers?</Label>
           </div>
         </div>
 
-        <div className="flex items-center gap-2">
-          <Label className="w-40">Vendor ordering code:</Label>
-          <div className="relative flex-1">
-            <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              id="sku"
-              placeholder="Might be similar to sku#"
-              className="h-9 w-72 pl-8"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Label className="w-40">Variant group code:</Label>
-          <div className="relative flex-1">
-            <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              id="sku"
-              placeholder="Mix and match items, same price"
-              className="h-9 w-72 pl-8"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Label className="w-40">Product Category:</Label>
-          <Select>
-            <SelectTrigger className="w-72">Select category</SelectTrigger>
-            <SelectContent>
-              {!categoryData ? (
-                <SelectItem value="loading"> Loading... </SelectItem>
-              ) : (
-                categoryData.allCategories.map((category) => (
-                  <SelectItem value={category.name} key={category.id}>
-                    {category.name}
-                  </SelectItem>
-                ))
-              )}
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div className="mt-2 flex items-center gap-2">
-          <div className="w-40">{""}</div>
+        {/* Bottom */}
+        <div className="flex  items-center gap-2 ">
+          <div className="hidden w-40 sm:inline-block">{""}</div>
           <Button className="max-w-xs" variant="secondary">
             Search
+          </Button>
+          <Button className="max-w-xs" variant="secondary">
+            Clear
+          </Button>
+          <Button className="max-w-xs" variant="secondary">
+            Pick Random
           </Button>
         </div>
       </form>
@@ -100,7 +193,7 @@ function ProductsSubLayout({ children }: { children: React.ReactNode }) {
           Product Maintenance
         </h1>
         <p className="mt-2 text-sm text-muted-foreground">
-          Manage your products and edit their details.
+          Fine grained search for all products in your inventory.
         </p>
       </div>
 
