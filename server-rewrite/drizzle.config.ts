c8d157b39a1b4071dd9e9fa459951bbfeed4e96e
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-05-16 22:23:23 -0700

    sql playground

--- server-rewrite/drizzle.config.ts
+++ server-rewrite/drizzle.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "drizzle-kit";
 
 const drizzleConfig = {
   schema: "./src/database/schemas/*",
-  out: "./src/database/previous-sql-scripts/",
+  out: "./src/database/sql-scripts/",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_CONNECTION_STRING!
---
+++ server-rewrite/src/database/sql-scripts/playground.sql
@@ -0,0 +1,5 @@
+SELECT * FROM acme_user;
+
+SELECT * FROM acme_category;
+
+SELECT * FROM acme_product;
