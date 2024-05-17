1b2d0bb9f1a13dacbfa9c070f0b4229687bdfd21
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-05-16 21:05:08 -0700

    database connection

--- server-rewrite/src/server.ts
+++ server-rewrite/src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
+import { logPostgresVersion } from "./database/db";
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, async () => {
   console.log(`Acme Ecommerce API running on port ${port}`);
+  await logPostgresVersion();
 });
