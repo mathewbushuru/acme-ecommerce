7feaf9c3c675d0e6e5053642b0a2873a113af8c5
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-05-17 05:15:47 -0700

    Seed script

--- server-rewrite/src/database/seed.ts
+++ server-rewrite/src/database/seed.ts
@@ -7,40 +7,39 @@ import {
   type NewProductType,
 } from "./schemas/product";
 
-const newUser: NewUserType = {
-  firstName: "Matt",
-  lastName: "B",
-  email: "matt@test.com",
-  hashedPassword: "Teter123",
-};
-
-const newCategory: NewCategoryType = {
-  name: "Test Category",
-};
-
-const newProduct: NewProductType = {
-  name: "Test product",
-  skuNumber: 12345,
-  regularPrice: "8.99",
-  vendorOrderingCode: "GT789",
-  categoryId: 1
-};
-
 export async function seedDatabase() {
+  await db.delete(user);
+  await db.delete(product);
+  await db.delete(category);
+
+  const newUser: NewUserType = {
+    firstName: "Matt",
+    lastName: "B",
+    email: "matt@test.com",
+    hashedPassword: "Tester123",
+  };
   const returnUserValue = await db.insert(user).values(newUser).returning();
   console.log(returnUserValue);
 
+  const newCategory: NewCategoryType = {
+    name: "Test Category",
+  };
   const returnCategoryValue = await db
     .insert(category)
     .values(newCategory)
     .returning();
   console.log(returnCategoryValue);
 
+  const newProduct: NewProductType = {
+    name: "Test product",
+    skuNumber: 12345,
+    regularPrice: "8.99",
+    vendorOrderingCode: "GT789",
+    categoryId: returnCategoryValue[0].id,
+  };
   const returnProductValue = await db
     .insert(product)
     .values(newProduct)
     .returning();
   console.log(returnProductValue);
 }
-
-// seedDatabase();
